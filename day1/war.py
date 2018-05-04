bda395792741201d4367999f7910a9e8baf39442
wanghengzhi <wanghengzhi@iautos.cn>
2018-05-04 23:53:29 +0800

    练习基础
    练习基础

--- day1/war.py
+++ day1/war.py
@@ -7,3 +7,50 @@
 """
 
 print('Hello World !')
+
+"""
+#website = 'http://www.baidu.com'
+"""
+
+
+def fibs(num):
+    result = [0, 1]
+    for i in range(num - 2):
+        result.append(result[-2] + result[-1])
+    return result
+
+
+a = fibs(10)
+print(a)
+print(fibs(100))
+
+name = "Hengzhi Wang"
+
+name2 = name
+print(name, name2)
+
+name = "Jack"
+
+print("What is the value of name2 now?", name, name2)
+
+"""
+name = input("What is your name?")
+print("Hello " + name)
+
+import getpass
+
+pwd = getpass.getpass("请输入密码：")
+
+print(pwd)
+"""
+import sys
+
+print(sys.argv)
+
+import os
+
+print(os.system("df -h"))
+
+import os, sys
+
+os.system(''.join(sys.argv[1:]))
